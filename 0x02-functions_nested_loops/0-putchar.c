863bb10205b65e07eb66747364c44e4b6a4847d7
Amrsalama97 <amrsalama97@gmail.com>
2023-07-18 15:16:05 -0700

    loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+*main - function prints text as output
+*/
+int main(void)
+{
+	char alx[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(alx[i]);
+	}
+	_putchar(10);
+	return (0);
+}
