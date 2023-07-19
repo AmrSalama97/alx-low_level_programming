f4cd0cf6c51d9817adc25d4fc4e927dc0f23d387
Amrsalama97 <amrsalama97@gmail.com>
2023-07-19 09:54:00 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,13 @@
 *this is a tester
 *for the positive_or_negative
 */
-
-int main (void)
+int main(void)
 {
-    int i ;
-    i=0   ;
-    positive_or_negative(i);
-    return (0)
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
+
