2b1f5a5579431d8ce21f65b07ea5e7935851a9e6
Amrsalama97 <amrsalama97@gmail.com>
2023-07-24 11:05:46 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+include "main.h"
+/**
+ * takes the variable 
+ * to a pointer 
+ * and update the value
+ */
+int main (void) 
+{
+	int n ;
+	n=402 ;
+	putchar('/'"n value is %d " ,n);
+	p=&n;
+	*p=402;
+	putchar ('/'"n value is %p ",n);
+	return (0);
+}
+
