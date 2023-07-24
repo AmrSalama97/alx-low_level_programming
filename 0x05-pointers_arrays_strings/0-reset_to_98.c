439d8f09d8bc789c8749c098b3cac036b09c473f
Amrsalama97 <amrsalama97@gmail.com>
2023-07-24 11:23:12 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,8 @@
 /**
 * reset_to_98 - updates the value of the variable
 * @n: pointer to the variable to update
-* takes the variable 
-* to a pointer 
-* and update the value
 */
 void reset_to_98(int *n)
 {
-    *n = 98;
+  *n = 98;
 }
