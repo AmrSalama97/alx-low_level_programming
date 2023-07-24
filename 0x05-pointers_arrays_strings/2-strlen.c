0febc6020bfa696de68b7684d5f8884fbabb3bc5
Amrsalama97 <amrsalama97@gmail.com>
2023-07-24 14:28:49 -0700

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-* int _strlen(char *s) - for string length
-* @s the string 
+*int _strlen(char *s) - for string length
+*@s the string 
 */
 
 int _strlen(char *s) 
 {
 	int length;
-	
-	length = 0;
 
+	length = 0;
 	while (s[length] != 0) 
 	{
 		length++; 
