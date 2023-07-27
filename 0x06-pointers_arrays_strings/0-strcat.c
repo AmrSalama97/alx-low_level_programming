5463cbf2bba55b0f89291eab592cedbed3dcb978
Amrsalama97 <amrsalama97@gmail.com>
2023-07-27 09:10:25 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * char -concatenates the strings 
+ * @dest -is the first string
+ * @src -is the appended string 
+ */
+
+char *_strcat(char *dest, char *src) 
+{
+	_putchar("%c %c ",dest,src);
+}
