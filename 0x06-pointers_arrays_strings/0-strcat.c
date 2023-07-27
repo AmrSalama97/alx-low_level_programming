6f50b3fde9be1b404bb2a4230b659a9bb70a6bfd
Amrsalama97 <amrsalama97@gmail.com>
2023-07-27 09:18:53 -0700

    more pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- * char -concatenates the strings 
+ * _strcat -concatenates the strings 
  * @dest -is the first string
  * @src -is the appended string 
  */
 
 char *_strcat(char *dest, char *src) 
 {
-	_putchar("%c %c ",dest,src);
+	int length, j;
+	length = 0;
+
+	while (dest[length]!= '\0')
+	{
+		length ++
+	}
+	for (j = 0; src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	return (dest);
 }
