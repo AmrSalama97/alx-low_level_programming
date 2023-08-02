bbb765b89adf4b2335fc0ce44b187c6ea32d6239
Amrsalama97 <amrsalama97@gmail.com>
2023-08-02 11:23:58 -0700

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,12 +5,12 @@
  * @n: integer
  *
  * Return: On success 1.
- * On error, 0 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return (0);
+		return (-1);
 	if (n == 0)
 		return (1);
 	return (n * factorial(n - 1));
