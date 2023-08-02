173361d6551451e33f36ba4d1d0239b417aade42
Amrsalama97 <amrsalama97@gmail.com>
2023-08-02 10:44:09 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_puts_recursion Function prints a string
+ *@s is the string
+ */
+
+void _puts_recursion(char *s)
+{
+        char c;
+	_putchar("%c",c);
+	_putchar('\n');
+	return (0);
+}
