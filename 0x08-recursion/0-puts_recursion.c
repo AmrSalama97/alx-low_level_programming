e96e1a0568e7882184bfe1e97dec7766b6d3917b
Amrsalama97 <amrsalama97@gmail.com>
2023-08-02 10:55:17 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
 
 void _puts_recursion(char *s)
 {
-        if (*s == 0)
+	if (*s == 0)
 	{
 		_putchar('\n');
 		return;
@@ -15,6 +15,6 @@ void _puts_recursion(char *s)
 	if (*s > 0)
 	{
 		_putchar (*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 }
