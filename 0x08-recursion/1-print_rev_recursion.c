7adc6e6808ec39771a5763e816f07a9527a7d227
Amrsalama97 <amrsalama97@gmail.com>
2023-08-02 11:01:04 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - prints the string in reverse
+ *@s: the reversed string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	if (*s > 0)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+}
+
