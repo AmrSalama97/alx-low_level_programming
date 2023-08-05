2612bef1ffcac1815f474d17f660c72237333fcb
Amrsalama97 <amrsalama97@gmail.com>
2023-08-04 18:02:37 -0700

    argc & argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Write a program that prints all the arguments received
  * All arguments should be printed, including the first on
@@ -7,7 +6,6 @@
  * @argv: this is the argument vector
  * Return: this is to return 0
  */
-
 int main(int args, char *argv[])
 {
 	int i;
