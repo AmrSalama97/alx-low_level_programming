bfef2f547a288221cc3bd23cffb57bcff367850a
Amrsalama97 <amrsalama97@gmail.com>
2023-08-04 17:28:10 -0700

    argc & argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,13 @@
  *main function - write the program name 
  *@argc : is the number of the arguments
  *@argv : is the address of argc which include the string values
- *
  * Return : this is return 0
  */
 
-int main (int argc ,char *argv[])
+int main (int argc,char *argv[])
 {
-	printf("%s\n",argv[0]);	
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
+
 }
