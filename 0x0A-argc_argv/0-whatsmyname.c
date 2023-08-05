be028b377e93b3012a0319f2ed59acb7ee7f8fd9
Amrsalama97 <amrsalama97@gmail.com>
2023-08-04 17:31:14 -0700

    argc & argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- *main function - write the program name 
- *@argc : is the number of the arguments
- *@argv : is the address of argc which include the string values
- * Return : this is return 0
+ * main - Write a program that prints its name, followed by a new line.
+ * If you rename the program, it will print the new name, without having to
+ *@argc: is the number of the arguments
+ *@argv: is the address of argc which include the string values
+ * Return: this return to 0
  */
 
-int main (int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	(void)argc;
 	return (0);
-
 }
