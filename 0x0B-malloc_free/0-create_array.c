6d3a60a397881d9e53590689ba4d9aea54d671aa
Amrsalama97 <amrsalama97@gmail.com>
2023-08-08 08:43:43 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,26 +1,29 @@
+#include "notrebloh.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
+
 /**
- *
- *
- *
+ * create_array - create an arry of size with char c
+ * @size: unsigned int type
+ * @c: char type
+ * Return: Return pointer to array
  */
 
 char *create_array(unsigned int size, char c)
 {
-	int i;
 	char *s;
+	unsigned int i;
+
 	if (size == 0)
-        {
-                _putchar("Null");
-                return (0);
-        }
-	s = malloc((size)*sizeof(char));
-	if (s == 0)
 	{
-		_putchar("Null");
-		return (0);
+		return (NULL);
+	}
+
+	s = malloc((size) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
 	}
 	i = 0;
 	while (i < size)
@@ -31,4 +34,3 @@ char *create_array(unsigned int size, char c)
 	s[i] = '\0';
 	return (s);
 }
-
