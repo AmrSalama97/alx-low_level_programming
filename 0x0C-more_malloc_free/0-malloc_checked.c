48ab280658f6afad0921d9d228dc965788e983c5
Amrsalama97 <amrsalama97@gmail.com>
2023-08-10 05:35:08 -0700

    malloc-free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,17 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * malloc_checked - allocates memory using malloc
- * @b: size of the memory block
+ * malloc_checked - allocates memory using malloc, exit(98) if it fails
+ * @b: size of the memory block to be allocated
+ *
  * Return: pointer to the address of the memory block
  */
 void *malloc_checked(unsigned int b)
 {
-	void *c;
+	void *block;
 
-	c = malloc(b);
-	if (c == Null)
+	block = malloc(b);
+	if (block == NULL)
 		exit(98);
-	return (c);
+	return (block);
 }
