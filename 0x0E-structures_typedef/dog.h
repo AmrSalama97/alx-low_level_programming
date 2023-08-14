6e4f740472d9adbc90954ba92a18e2e120654ae6
AmrSalama97 <AmrSalama97@gmail.com>
2023-08-14 12:35:49 -0700

    Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - such structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
