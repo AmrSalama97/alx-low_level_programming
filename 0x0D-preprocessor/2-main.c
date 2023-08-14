7372c58b704492c66be968951e2d750fc4e51011
AmrSalama97 <AmrSalama97@gmail.com>
2023-08-14 11:57:03 -0700

    Macros

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file 
- * Return: everything is ok
+ * main - prints the name of the file it was compiled from
+ * Return: (0) everything worked ok
  */
 
 int main(void)
